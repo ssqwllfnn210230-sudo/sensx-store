449319c2bc89d30eded52a3c3efd362fbed488ab
ssqwllfnn210230-sudo <ssqwllfnn210230@gmail.com>
2026-04-03 18:26:12 +0200

    Refactor index.js layout and styling

--- pages/index.js
+++ pages/index.js
@@ -30,42 +30,87 @@ export default function Home() {
   }
 
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      
-      {/* КНОПКА В КОРЗИНУ */}
-      <div style={{ marginBottom: "20px" }}>
-        <a href="/cart">
-          <button>
-            🛒 Корзина ({cart.reduce((sum, item) => sum + item.count, 0)})
-          </button>
-        </a>
-      </div>
+    <div style={{
+      background: "#fff",
+      minHeight: "100vh",
+      fontFamily: "Arial",
+      padding: "20px"
+    }}>
+
+      {/* ЛОГО */}
+      <h1 style={{
+        textAlign: "center",
+        fontWeight: "bold",
+        letterSpacing: "2px"
+      }}>
+        SENSX
+      </h1>
 
       {/* ТОВАР */}
-      <div
-        style={{
-          maxWidth: "400px",
-          border: "1px solid #ddd",
-          padding: "15px",
-          borderRadius: "10px"
-        }}
-      >
+      <div style={{
+        maxWidth: "350px",
+        margin: "40px auto",
+        textAlign: "left"
+      }}>
         <img
           src="https://i.imgur.com/3QZQZQy.png"
           style={{
             width: "100%",
-            borderRadius: "10px"
+            borderRadius: "12px"
           }}
         />
 
-        <h2>Кофта SensX</h2>
-        <p style={{ color: "#555" }}>Чёрная стильная кофта</p>
-        <p style={{ fontWeight: "bold" }}>1500 ₽</p>
+        <h2 style={{ marginTop: "15px" }}>
+          Кофта SensX
+        </h2>
 
-        <button onClick={addToCart}>
+        <p style={{ color: "#666" }}>
+          Чёрная кофта с капюшоном
+        </p>
+
+        <p style={{
+          fontWeight: "bold",
+          fontSize: "18px"
+        }}>
+          1500 грн
+        </p>
+
+        <button
+          onClick={addToCart}
+          style={{
+            width: "100%",
+            padding: "12px",
+            background: "black",
+            color: "white",
+            border: "none",
+            borderRadius: "8px",
+            marginTop: "10px"
+          }}
+        >
           Добавить в корзину
         </button>
       </div>
+
+      {/* КОРЗИНА */}
+      <a href="/cart">
+        <div style={{
+          position: "fixed",
+          bottom: "20px",
+          right: "20px",
+          background: "black",
+          color: "white",
+          width: "60px",
+          height: "60px",
+          borderRadius: "50%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: "20px"
+        }}>
+          🛒 {cart.reduce((sum, item) => sum + item.count, 0)}
+        </div>
+      </a>
+
     </div>
   );
-          }
+}
